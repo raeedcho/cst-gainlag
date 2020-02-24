f4d4736cbe2a6f39a44a124a05989bfd2d1a6b74
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2020-02-24 17:49:02 -0500

    feat(batch convert): add script to batch convert files

---
+++ batch_convert_cst.m
@@ -0,0 +1,33 @@
+%% Set up
+dataroot = '/mnt/smile-share/Animals/';
+monkey = 'Ford';
+fid = fopen('ford-cst-dates.txt');
+dates = textscan(fid,'%s');
+fclose(fid);
+dates=dates{1};
+savedir = '/data/raeed/project-data/smile/cst-gainlag/library';
+
+%% go through files
+for filenum = 1:length(dates)
+    file_info = dir(fullfile(...
+        dataroot,...
+        monkey,...
+        dates{filenum}(1:4),...
+        dates{filenum}(5:6),...
+        dates{filenum},...
+        'handControl',...
+        sprintf('%s*.mat',monkey)));
+
+    if length(file_info)>1
+        fprintf('Multiple matching files for date %s, skipping to next...\n',dates{filenum})
+        continue
+    elseif isempty(file_info)
+        fprintf('No file for date %s, skipping to next\n',dates{filenum})
+        continue
+    end
+
+    smile_data = load(fullfile(file_info.folder,file_info.name));
+
+    trial_data = convertSMILEtoTD(smile_data.Data,struct('array_alias',{{'Right M1','M1'}}));
+    save(fullfile(savedir,sprintf('%s_%s_TD.mat',monkey,dates{filenum})),'trial_data')
+end
