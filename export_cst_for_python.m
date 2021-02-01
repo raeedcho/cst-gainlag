6b9fbdfe622dbcc7b325e7b93a0ab4a139c384bb
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2021-02-01 13:20:27 -0600

    feat(python export): add script to export data for python

---
+++ export_cst_for_python.m
@@ -0,0 +1,49 @@
+%% Set up
+    dataroot = '/data/raeed/project-data/smile/cst-gainlag';
+    if ispc
+        dataroot = 'C:\Users\Raeed\data\project-data\smile\cst-gainlag';
+    end
+
+    file_info = dir(fullfile(dataroot,'library','*COCST*'));
+    filenames = horzcat({file_info.name})';
+    
+%% Loop through files
+filetic = tic;
+for filenum = 27%length(filenames)
+    td_cst = load_clean_cst_data(fullfile(dataroot,'library',filenames{filenum}));
+    
+%     % get rid of unsorted neurons
+%     bad_units = td(1).M1_unit_guide(:,2)<=1;
+%     % for file 31 only! remove neurons 8,2 and 64,2
+%     if contains(td(1).date_time,'2018/06/26')
+%         corr_units = [8 2;64 2];
+%         bad_units = bad_units | ismember(td(1).M1_unit_guide,corr_units,'rows');
+%     end
+%     for trialnum = 1:length(td)
+%         td(trialnum).M1_spikes = td(trialnum).M1_spikes(:,~bad_units);
+%         td(trialnum).M1_unit_guide = td(trialnum).M1_unit_guide(~bad_units,:);
+%     end
+%     
+%     if isempty(td(1).M1_unit_guide)
+%         fprintf('Skipping file %d because no spike data...\n',filenum)
+%         continue
+%     end
+%     
+    % Make sure we have CST trials
+    if isempty(td_cst)
+        fprintf('Incomplete dataset for file %d\n',filenum)
+        continue
+    end
+    
+    % prep TD for phase plotting
+    td_cst = trimTD(td_cst,'idx_cstStartTime','idx_cstEndTime');
+    
+    % add timevector to each trial
+    for trialnum = 1:length(td_cst)
+        td_cst(trialnum).trialtime = ((1:length(td_cst(trialnum).hand_pos))-1)'*td_cst(trialnum).bin_size;
+    end
+    
+    save(fullfile(dataroot,'library','python',filenames{filenum}),'td_cst')
+
+    fprintf('Finished file %d of %d at time %f\n',filenum,length(filenames),toc(filetic))
+end
