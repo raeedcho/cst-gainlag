5d444bab9a2ce3511e1eb0b7cffaf635733d945c
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2021-06-22 19:13:36 -0400

    feat(load): offload some data processing from each load time to the time loading from raw

--- lib/load_clean_cst_data.m
+++ lib/load_clean_cst_data.m
@@ -27,45 +27,41 @@ function [td] = load_clean_cst_data(filename,params)
     td(abort_idx) = [];
     fprintf('Removed %d trials that monkey aborted\n',sum(abort_idx))
 
-    % trim nans off the end of trials for cursor pos and hand pos
+    % trim nans off the end of trials for hand pos
     for trialnum = 1:length(td)
-        nan_times = any(isnan(getSig(td(trialnum),getTDfields(td,'time'))),2);
+        nan_times = any(isnan(getSig(td(trialnum),'hand_pos')),2);
         first_viable_time = find(~nan_times,1,'first');
         last_viable_time = find(~nan_times,1,'last');
         td(trialnum) = trimTD(td(trialnum),{'start',first_viable_time-1},{'start',last_viable_time-1});
     end
 
-    % fill in CST windows (go cue and reward time seem to be off by some random amount)
-    cst_idx = getTDidx(td,'task','CST');
-    for trialnum = 1:length(cst_idx)
-        trial_idx = cst_idx(trialnum);
-        cst_window = td(trial_idx).cursor_pos(:,1)~=td(trial_idx).hand_pos(:,1);
-        td(trial_idx).idx_cstStartTime = find(cst_window,1,'first');
-        td(trial_idx).idx_cstEndTime = find(cst_window,1,'last');
-    end
+%     % fill in CST windows (go cue and reward time seem to be off by some random amount)
+%     cst_idx = getTDidx(td,'task','CST');
+%     for trialnum = 1:length(cst_idx)
+%         trial_idx = cst_idx(trialnum);
+%         cst_window = td(trial_idx).cursor_pos(:,1)~=td(trial_idx).hand_pos(:,1);
+%         td(trial_idx).idx_cstStartTime = find(cst_window,1,'first');
+%         td(trial_idx).idx_cstEndTime = find(cst_window,1,'last');
+%     end
+%     
+%     % fill in CO target directions
+%     for trialnum = 1:length(td)
+%         if strcmpi(td(trialnum).task,'CO')
+%             td(trialnum).tgtDir = atan2d(...
+%                 td(trialnum).ot_location(2)-td(trialnum).ct_location(2),...
+%                 td(trialnum).ot_location(1)-td(trialnum).ct_location(1));
+%         else
+%             td(trialnum).tgtDir = NaN;
+%         end
+%     end
     
-    % fill in CO target directions
-    for trialnum = 1:length(td)
-        if strcmpi(td(trialnum).task,'CO')
-            td(trialnum).tgtDir = atan2d(...
-                td(trialnum).ot_location(2)-td(trialnum).ct_location(2),...
-                td(trialnum).ot_location(1)-td(trialnum).ct_location(1));
-        else
-            td(trialnum).tgtDir = NaN;
-        end
-    end
-    
-    % fill kinematic signals (filter ahead of differentials)
-    samp_rate = 1/td(1).bin_size;
-    [filt_b,filt_a] = butter(16,cutoff_freq/(samp_rate/2));
-    td = filterSignals(td,struct('signals','hand_pos','filt_a',filt_a,'filt_b',filt_b));
+    % fill kinematic signals
     td = getDifferential(td,struct('signals','hand_pos','alias','hand_vel'));
     td = getDifferential(td,struct('signals','hand_vel','alias','hand_acc'));
-    td = getDifferential(td,struct('signals','cursor_pos','alias','cursor_vel'));
 
     % add shifted signals
     shift_bins = floor(-0.1/td(1).bin_size);
-    td = dupeAndShift(td,'cursor_pos',shift_bins,'cursor_vel',shift_bins);
+    td = dupeAndShift(td,'cst_cursor_pos',shift_bins,'cst_cursor_vel',shift_bins);
     
     % neural data stuff
     if ~isempty(td(1).M1_unit_guide)
